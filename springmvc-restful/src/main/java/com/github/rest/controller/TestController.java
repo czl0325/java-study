e928bc8037194a10b51f61636932a38cb371e65f
陈昭良 <295183917@qq.com>
2022-05-06 17:27:13 +0800

    新建项目

--- springmvc-restful/src/main/java/com/github/rest/controller/TestController.java
+++ springmvc-restful/src/main/java/com/github/rest/controller/TestController.java
@@ -1,13 +1,15 @@
 package com.github.rest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
-    @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/test")
+    public String testIndex(Model model) {
+        System.out.println("进入断点");
+        return "test";
     }
 }
